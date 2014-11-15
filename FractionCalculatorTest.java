6198f5311b8922228eb572561baed802e10ccdfb
BBK-PiJ-2014-66 <osmart01@dcs.bbk.ac.uk>
2014-11-15 22:27:53 +0000

    Mini program invoke test in FractionCalculator... See FractionCalculator.java commit message for record of the program output.

---
+++ FractionCalculatorTest.java
@@ -0,0 +1,15 @@
+/*
+ * Birbeck MSc Computer Science PiJ Coursework Two
+ * author: Oliver S. Smart
+ * date: from 15 Nov 2014
+ *  
+ * Run the tests of FractionCalculator function as
+ * defined in the FractionCalculator.simpleTests() method.
+ * run verbose.
+ *  
+ */ 
+public class FractionCalculatorTest{
+	public static void main( String args[]) {
+		FractionCalculator.simpleTests( true); // run in verbose mode
+	}
+}
