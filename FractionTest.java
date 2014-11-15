7702f9c34634f13312e022d60b6b376c9345efb0
Oliver Smart <osmart01@dcs.bbk.ac.uk>
2014-11-15 12:37:46 +0000

    Rewrite FractionTest, program output ... Rewritten to be rather clearer!
    Program output with -ea option to turn on assert test
    
    $ java -ea FractionTest
    FractionTest tests verbose output:
    Invalid fraction with denominator 0
    ERROR FractionTest FAILS for test "That 1/0 should throw an exception".
    test "That multiplying 1/2 by 3/5 gives 3/10" passes.
    test "That 1/2 .equals 1/2" passes.
    test "That 1/2 .equals 3/6" passes.
    test "That -1/2 .equals 1/-2" passes.
    test "That -1/-2 .equals 1/2" passes.
    test "That 4/-8 does not .equals 1/2" passes.
    Exception in thread "main" java.lang.AssertionError: FractionTest failed 1 tests
    	at FractionTest.simpleAssertTests(FractionTest.java:68)
    	at FractionTest.main(FractionTest.java:22)

--- FractionTest.java
+++ FractionTest.java
@@ -6,30 +6,76 @@
  * Based on:
  * "Created by keith for the second coursework assignment."
  *
- * runs test on the Fraction class.
+ * Runs test on the Fraction class. 
+ *
+ * 	- Implement afresh using a method rather than in main. 
+ *	  Idea is to allow tests to be run in programs that use the 
+ *	  class during development. If any problems found then throw
+	  an assert failure as this allows user to turn on/off.
+ *	- Replace original "test" method taking two equalities
+ *	  with one that is like assert that takes a boolean.
+ *	  this allows testing for negatives...
+ * 
  */
 public class FractionTest {
-    public static void main(String[] args) {
+	public static void main(String[] args) {
+		simpleAssertTests(true);  // run verbose
+	}
+ 	/* run a series of tests. If beVerbose set true give pass output
+	   but normally just output errors. 
+	   At end if any error has been found throw an assert failure */
+	public static void simpleAssertTests(boolean beVerbose) {
+		if (beVerbose) 
+			 System.out.println("FractionTest tests verbose output:");
+		int numbFails = 0;
+		boolean testPass;
+		String DescribeTest;
+		
+		DescribeTest = "That 1/0 should throw an exception";
+                try {
+			new Fraction(1,0); 
+			testPass = false;
+		} catch (Exception e) { /* N.B. catching general exception is bad practice: 
+                                           http://stackoverflow.com/questions/1075895/how-can-i-catch-all-the-exceptions-that-will-be-thrown-through-reading-and-writi */
+			testPass = true; 
+		}
+		numbFails += likeAssert( testPass, DescribeTest, beVerbose);
 
-        // test divide by zero - should print an error and exit
-        new Fraction(1, 0);
-        // test multiply
-	Fraction f = new Fraction(3,10);
-	Fraction g = new Fraction(1,2);
-	Fraction h = new Fraction(3,5);
-	if (!f.equals(g.multiply(h))) System.out.println("Multiply failed");
-        // test equals
-	test(new Fraction(1, 2),new Fraction(1, 2),"error test 1");
-	test(new Fraction(1, 2),new Fraction(3, 6),"error test 2");
-	test(new Fraction(-1, 2),new Fraction(1, -2),"error test 3");
-	test(new Fraction(-1, -2),new Fraction(1, 2),"error test 4");
-	test(new Fraction(4, -8),new Fraction(1, 2),"error test 5");
+		DescribeTest = "That multiplying 1/2 by 3/5 gives 3/10";
+               	testPass = (new Fraction(1,2).multiply(new Fraction(3,5))).equals(new Fraction(3,10)); 
+		numbFails += likeAssert( testPass, DescribeTest, beVerbose);
 
-        // extend with extra tests
-    }
+		DescribeTest = "That 1/2 .equals 1/2";
+		testPass =  (new Fraction(1,2).equals(new Fraction(1,2)));
+		numbFails += likeAssert( testPass, DescribeTest, beVerbose);
 
-    static void test(Fraction f1, Fraction f2, String msg){
-    	if (! f1.equals(f2))
-		System.out.println(msg);
-    }
+		DescribeTest = "That 1/2 .equals 3/6";
+		testPass =  (new Fraction(1,2).equals(new Fraction(3,6)));
+		numbFails += likeAssert( testPass, DescribeTest, beVerbose);
+
+		DescribeTest = "That -1/2 .equals 1/-2";
+		testPass =  (new Fraction(-1,2).equals(new Fraction(1,-2)));
+		numbFails += likeAssert( testPass, DescribeTest, beVerbose);
+
+		DescribeTest = "That -1/-2 .equals 1/2";
+		testPass =  (new Fraction(-1,-2).equals(new Fraction(1,2)));
+		numbFails += likeAssert( testPass, DescribeTest, beVerbose);
+
+		DescribeTest = "That 4/-8 does not .equals 1/2";
+		testPass =  !(new Fraction(4,-8).equals(new Fraction(1,2)));
+		numbFails += likeAssert( testPass, DescribeTest, beVerbose);
+
+		assert (numbFails==0) : "FractionTest failed "+ numbFails + " tests";
+
+ 
+	}
+	private static int likeAssert( boolean testPass, String describeTest, boolean beVerbose) {
+		if (!testPass) {
+			System.out.println("ERROR FractionTest FAILS for test \"" +  describeTest + "\".");
+			return 1;
+		} else if (beVerbose) {
+			System.out.println("test \"" +  describeTest + "\" passes.");
+	 	}
+		return 0;
+	}
 }
--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-cw2
-===
+Coursework Two
+==============
+
+From Programming in Java course http://www.dcs.bbk.ac.uk/courses/modules/BUCI033S7.php
+
+- The coursework sheet [cw2.pdf](cw2.pdf) 
+- [FractionTest.java](FractionTest.java) tests for Fraction class. N.B. program output given
+  in the git commit message during each stage of development. 
+
+
+Oliver Smart
+
