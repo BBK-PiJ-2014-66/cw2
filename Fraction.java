6983980c6db3d82537e0ed8171bae3e9833bde71
BBK-PiJ-2014-66 <osmart01@dcs.bbk.ac.uk>
2014-11-15 13:33:18 +0000

    Change initial comment & stick through eclipse ... to sort out identation. Not yet altered any functionality.

--- Fraction.java
+++ Fraction.java
@@ -1,75 +1,86 @@
-/**
- * Created by keith for the second coursework assignment.
+/*
+ * Birbeck MSc Computer Science PiJ Coursework Two
+ * author Oliver S. Smart
+ * date from 15 Nov 2014
+ *  
+ * Based on:
+ * "Created by keith for the second coursework assignment."
+ *
+ * Performs fraction calculations.
  */
 public class Fraction {
-    private int numerator;
-    private int denominator;
+	private int numerator;
+	private int denominator;
 
-    public Fraction(int num, int denom) {
-        if (denom == 0) {
-            System.out.println("Invalid fraction with denominator 0"); 
-	    // this should use exceptions
-            return;
-        }
-        int gcd = myGcd(num, denom);
-        setNumerator(num / gcd);
-        setDenominator(denom / gcd);
-    }
+	public Fraction(int num, int denom) {
+		if (denom == 0) {
+			System.out.println("Invalid fraction with denominator 0");
+			// this should use exceptions
+			return;
+		}
+		int gcd = myGcd(num, denom);
+		setNumerator(num / gcd);
+		setDenominator(denom / gcd);
+	}
 
-    @Override
-    public String toString() {
-        return "" + getNumerator() + '/' + getDenominator();
-    }
+	@Override
+	public String toString() {
+		return "" + getNumerator() + '/' + getDenominator();
+	}
 
-    public int getNumerator() {
-        return numerator;
-    }
+	public int getNumerator() {
+		return numerator;
+	}
 
-    public void setNumerator(int num) {
-        numerator = num;
-    }
+	public void setNumerator(int num) {
+		numerator = num;
+	}
 
-    public int getDenominator() {
-        return denominator;
-    }
+	public int getDenominator() {
+		return denominator;
+	}
 
-    public void setDenominator(int num) {
-        denominator = num;
-    }
+	public void setDenominator(int num) {
+		denominator = num;
+	}
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+	@Override
+	public boolean equals(Object o) {
+		if (this == o)
+			return true;
+		if (o == null || getClass() != o.getClass())
+			return false;
 
-        Fraction fraction = (Fraction) o;
+		Fraction fraction = (Fraction) o;
 
-        if (getDenominator() != fraction.getDenominator()) return false;
-        if (getNumerator() != fraction.getNumerator()) return false;
+		if (getDenominator() != fraction.getDenominator())
+			return false;
+		if (getNumerator() != fraction.getNumerator())
+			return false;
 
-        return true;
-    }
+		return true;
+	}
 
-    @Override
-    public int hashCode() {
-        int result = numerator;
-        result = 31 * result + denominator;
-        return result;
-    }
+	@Override
+	public int hashCode() {
+		int result = numerator;
+		result = 31 * result + denominator;
+		return result;
+	}
 
-    public Fraction multiply(Fraction other) {
+	public Fraction multiply(Fraction other) {
 
-        int num = this.getNumerator() * other.getNumerator();
-        int denom = this.getDenominator() * other.getDenominator();
-        return new Fraction(num, denom);
-    }
+		int num = this.getNumerator() * other.getNumerator();
+		int denom = this.getDenominator() * other.getDenominator();
+		return new Fraction(num, denom);
+	}
 
-    private int myGcd(int a, int b) {
-        while (b != 0) {
-            int t = b;
-            b = a % b;
-            a = t;
-        }
-        return a;
-    }
+	private int myGcd(int a, int b) {
+		while (b != 0) {
+			int t = b;
+			b = a % b;
+			a = t;
+		}
+		return a;
+	}
 }
