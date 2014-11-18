dcd84a3b08a80d8ff278ec0b192c72610a4a95cc
BBK-PiJ-2014-66 <osmart01@dcs.bbk.ac.uk>
2014-11-18 11:15:17 +0000

    FracCalcOliver* initial commit. Why: ... ... I had "mis-read" course work FractionCalculator is REQUIRED to have a method evaluate that returns a Fraction (Keith told me last night).
    So take existing rather elegant (I think) FractionCalculator.java
    and duplicate it as FracCalcOliver.java
    
    Similarly duplicate existing FractionCalculatorTest.java
    as  FracCalcOliverTest.java
    
    N.B. Neither will compile:
    
    $ javac FracCalc*.java
    FracCalcOliver.java:34: class FractionCalculator is public, should be declared in a file named FractionCalculator.java
    public class FractionCalculator {
           ^
    FracCalcOliverTest.java:10: class FractionCalculatorTest is public, should be declared in a file named FractionCalculatorTest.java
    public class FractionCalculatorTest{
           ^
    2 errors
    
    because I have not sorted out the wrong class names yet

---
+++ FracCalcOliver.java
@@ -0,0 +1,272 @@
+/*
+ * Birbeck MSc Computer Science PiJ Coursework Two
+ * author: Oliver S. Smart
+ * date: from 15 Nov 2014
+ *  
+ * Simple text-based calculator to compute with fractions.
+ *
+ * Going to do something a bit-different from the exercise sheet.
+ * It seems a bit strange that the calculator "remembers" the
+ * fraction after a new line but forgets the operation. Surely
+ * it would be more elegant to remember both. Also want a clean
+ * way to return after user has asked for a quit.
+ * 
+ * Have a void "process" object method (rather than "evaluate") this
+ * will take the user user input as inputString. 
+ *
+ * Use methods to access object variables to: 
+ *  - give access to output message outputString
+ *  - give access to a boolean quitProgram that says when user wants to quit
+ *  - give access to a boolean foundError. Useful for testing but not 
+ *    needed for actual operation.
+ * 
+ * Other object variables will remember the complete state (including 
+ * "remember operation") between different input lines. 
+ *
+ * Note there is no need for any public access to the actual resulting 
+ * fraction (but it would be easy to add).
+ *
+ * This approach should enable full testing for instance that entering "0/1"
+ * produces an error message. (The "evaluate" method described in the exercise 
+ * sheet involves direct printing of error message so precluding assert testing).
+ *
+ */
+public class FractionCalculator {
+	private String outputString;
+	private boolean quitProgram;
+	private boolean foundError;
+	private Fraction valueInCalculator;
+	private String rememberedOperation;
+
+ 	private static final String MULTIPLY = "*";
+	private static final String DIVISION = "/";
+	private static final String ADDITION = "+";
+	private static final String SUBTRACT = "-";
+	private static final String NONE = "";
+
+
+	public static void main( String args[]) {
+		System.out.println("Simple text-based calculator to compute with fractions.\n" +
+			"author: Oliver S. Smart");
+		FractionCalculator myFractCalc = new FractionCalculator();
+		String prompt = " >>> ";
+		String currentPrompt = "0" + prompt;
+		while (true) {
+			System.out.print(currentPrompt);
+			String userInput = System.console().readLine();
+			myFractCalc.process(userInput);
+			if (myFractCalc.quitProgram()) break;
+			if (myFractCalc.foundError()) {
+				System.out.println(myFractCalc.outputString());
+				currentPrompt = "0" + prompt;
+			} else {
+				currentPrompt = myFractCalc.outputString() + prompt;	
+				
+			}
+		}
+		System.out.println("Goodbye"); 
+	}
+
+	public FractionCalculator() {
+		valueInCalculator = new Fraction(0, 1);
+		quitProgram = false;
+		foundError = false;
+		outputString = ""; // avoid null because want to use .equals
+		rememberedOperation = NONE;
+	}
+
+	public String outputString() {
+		return outputString;
+	}
+
+	public boolean quitProgram() {
+		return quitProgram;
+	}
+
+	public boolean foundError() {
+		return foundError;
+	}
+
+	public void process( String inputString) {
+		/* need to parse inputString as space separated "words".
+		   Could write a DIY for loop to go through the String character by character
+		   but I want a method like perl split to split a string into an array of words
+                   on a regular expression. Use String .split(regex) useful page:
+		   http://www.vogella.com/tutorials/JavaRegularExpressions/article.html
+                   */
+		quitProgram = false;
+		foundError = false;
+		outputString = ""; 
+		/* uncomment following line if we want to have a calculator that does not carry 
+                   memory for current operation between lines (as in the exercise sheet) */ /*
+                rememberedOperation = NONE; */
+
+                if (inputString!=null) { // to protect against EOF (ctrl-D) entry
+			String[] words = inputString.split("\\s+"); // one or more white space characters
+			for (int wc=0; wc < words.length; wc++) {
+				this.processAWord(words[wc]);
+				if (foundError) 
+					resetCalculator();
+				if (foundError || quitProgram) 
+					return;
+			}
+		}
+		/* normally return the string of the value in the calculator 
+                   and any current operator */
+		outputString = valueInCalculator.toString(); 
+		if (!rememberedOperation.equals(NONE)) 
+			outputString += " " + rememberedOperation;
+		return;
+	}
+
+	private void processAWord( String word) {
+		if (word.length() == 0) { // ignore any blank words
+		} else if (this.operatorProcess(word)) {
+		} else if (this.fractionProcess(word)) {
+		} else if (this.wholeNumberProcess(word)) {
+		} else if (this.absProcess(word)) {
+		} else if (this.negProcess(word)) {
+		} else if (this.clearProcess(word)) {
+		} else if (this.quitProcess(word)) {
+		} else {
+			this.unRecognized(word);
+		}
+	}
+
+	private boolean operatorProcess( String word) {
+		/* recognizes an operators, process appropriately 
+		   and return true */
+		if ( word.equals(MULTIPLY) || word.equals(DIVISION) ||
+		     word.equals(ADDITION) || word.equals(SUBTRACT) ) {
+			if (!rememberedOperation.equals(NONE)) {
+				outputString = "\nERROR you have input two operators: ";	
+				outputString += rememberedOperation + " and ";
+				outputString += word + ". ";
+				foundError = true;
+			}
+			rememberedOperation = word;
+			return true;
+		}
+		return false;
+	}
+
+	private boolean fractionProcess( String word) {
+		/* regex in matches:
+                 * means a number followed by '/' followed by a number
+                 * where a number is 0 or 1 '-' followed by one or more digits  
+                 */
+		if (word.matches("-?\\d+/-?\\d+")) { 
+			/* now want to get the numerator and denominator use a split on /
+			 * to get array and Integer.parseInt to convert. I think the regular expression
+                         *  should mean that no errors can occur (famous last words 
+                         */
+			int numerator = Integer.parseInt(word.split("/")[0]); 
+			int denominator = Integer.parseInt(word.split("/")[1]); 
+			// denominator not allowed to be zero
+			if (denominator== 0) {
+				outputString = "\nERROR you have specified a fraction " + word + " with a zero denominator.";	
+				foundError = true;
+			} else {
+				// use a separate function to actually deal with the fraction as need to do same will whole number
+				dealWithFraction(  new Fraction( numerator, denominator));	
+			}
+			return true;	
+		}
+		return false;
+	}
+	private void dealWithFraction( Fraction inFraction) {
+		if (!rememberedOperation.equals(NONE)) {
+			// apply operator
+			if (rememberedOperation.equals(MULTIPLY)) {
+				valueInCalculator = valueInCalculator.multiply(inFraction);
+			} else if (rememberedOperation.equals(DIVISION)) {
+				if (inFraction.equals(new Fraction(0,1)) ) {
+					outputString = "\nERROR you cannot divide by zero.";	
+					foundError = true;
+					return;
+				}
+                                valueInCalculator = valueInCalculator.divide(inFraction);
+                        } else if (rememberedOperation.equals(ADDITION)) {
+                                valueInCalculator = valueInCalculator.add(inFraction);
+                        } else if (rememberedOperation.equals(SUBTRACT)) {
+                                valueInCalculator = valueInCalculator.subtract(inFraction);
+                        } else {
+				assert (false) : "internal logic ERROR unrecognized rememberedOperation= " + rememberedOperation;
+			}
+			rememberedOperation = NONE; // clear operator
+		} else {
+			valueInCalculator = inFraction;
+		}
+
+	}
+
+	private boolean wholeNumberProcess( String word) {
+		if (word.matches("-?\\d+")) { // 0 or 1 - followed by one or more digits
+			int numerator = Integer.parseInt(word); 
+			dealWithFraction(  new Fraction( numerator, 1));	
+			return true;	
+		}
+		return false;
+	}
+
+	private boolean absProcess( String word) {
+		if (matchCaseInsensitiveAbbrev("abs",word)) {
+			valueInCalculator = valueInCalculator.absValue();	
+			return true;
+		}
+		return false;
+	}
+
+	private boolean negProcess( String word) {
+		if (matchCaseInsensitiveAbbrev("neg",word)) {
+			valueInCalculator = valueInCalculator.negate();	
+			return true;
+		}
+		return false;
+	}
+
+	private boolean clearProcess( String word) {
+		if (matchCaseInsensitiveAbbrev("clear",word)) {
+			valueInCalculator = new Fraction(0, 1);
+			rememberedOperation = NONE;
+			return true;
+		}
+		return false;
+	}
+
+	private boolean quitProcess( String word) {
+		if (matchCaseInsensitiveAbbrev("quit",word)) {
+			quitProgram = true;
+			return true;
+		}
+		return false;
+	}
+
+	private void unRecognized( String word) {
+		outputString = "\nERROR unrecognized input '" + word + "' cannot process this";	
+		foundError = true;
+	}
+
+	private void resetCalculator() {
+		valueInCalculator = new Fraction(0, 1);
+		rememberedOperation = NONE;
+		outputString += "\n\tResetting calculator to its initial state (no operator and 0 as value)";
+	}
+
+	private static boolean matchCaseInsensitiveAbbrev( String key,  String word) {
+		/* Returns true if word matches key (no regex allowed). 
+		 * The match is case insensitive and abbreviation is allowed.
+		 * So if key="abs" then get match for words:
+                 * "A", "a", "AB", "aB", "Ab", ...                            
+                 */
+		key = key.toLowerCase();
+		word = word.toLowerCase();
+		if (word.length() < key.length()) {
+			key = key.substring(0,word.length());
+		}
+		if (word.equals(key))
+			return true;
+		return false;
+	}
+
+}
---
+++ FracCalcOliverTest.java
@@ -0,0 +1,131 @@
+/*
+ * Birbeck MSc Computer Science PiJ Coursework Two
+ * author: Oliver S. Smart
+ * date: from 15 Nov 2014
+ *  
+ * Runs tests of FractionCalculator function and provides an 
+ * assert type test.
+ *  
+ */ 
+public class FractionCalculatorTest{
+	public static void main( String args[]) {
+		/* if assert is being used run the FractionTest to test Fraction
+                   as argument is false then non-verbose will be 
+                   silent unless there is an error */
+		assert FractionTest.simpleAssertTests(false) : "FractionTest failed";
+		simpleTests( true); // run in verbose mode
+	}
+
+	public static boolean simpleTests( boolean beVerbose) {
+		if (beVerbose)
+                         System.out.println("FractionTestCalculator tests verbose output:");
+
+		FractionCalculator testFracCalc = new FractionCalculator();
+
+		int numbFails = 0;
+		boolean testPass;
+		String DescribeTest;
+		String lineToProcess; 
+
+		lineToProcess = "1/2 * 3/4";
+		DescribeTest = "Simple parsing and operation: input of '" + lineToProcess + "' should give '3/8'. ";
+		testFracCalc.process(lineToProcess);
+		testPass = testFracCalc.outputString().equals("3/8");
+		numbFails += FractionTest.likeAssert( testPass, DescribeTest, beVerbose);
+
+		lineToProcess = "1/2 / 3/4";
+		DescribeTest = "Simple parsing and operation: input of '" + lineToProcess + "' should give '2/3'. ";
+		testFracCalc.process(lineToProcess);
+		testPass = testFracCalc.outputString().equals("2/3");
+		numbFails += FractionTest.likeAssert( testPass, DescribeTest, beVerbose);
+
+		lineToProcess = " 1/2   +  -1/4 ";
+		DescribeTest = "Simple parsing and operation: input of '" + lineToProcess + "' should give '1/4'. ";
+		testFracCalc.process(lineToProcess);
+		testPass = testFracCalc.outputString().equals("1/4");
+		numbFails += FractionTest.likeAssert( testPass, DescribeTest, beVerbose);
+
+		lineToProcess = "1/2 - 3/4";
+		DescribeTest = "Simple parsing and operation: input of '" + lineToProcess + "' should give '-1/4'. ";
+		testFracCalc.process(lineToProcess);
+		testPass = testFracCalc.outputString().equals("-1/4");
+		numbFails += FractionTest.likeAssert( testPass, DescribeTest, beVerbose);
+
+		lineToProcess = "-2/-3";
+		DescribeTest = "Handling fractions with -ve denominator: input of '" + lineToProcess + "' should give '2/3'. ";
+		testFracCalc.process(lineToProcess);
+		testPass = testFracCalc.outputString().equals("2/3");
+		numbFails += FractionTest.likeAssert( testPass, DescribeTest, beVerbose);
+
+		lineToProcess = "3 * 9";
+		DescribeTest = "Whole numbers: input of '" + lineToProcess + "' should give '27'. ";
+		testFracCalc.process(lineToProcess);
+		testPass = testFracCalc.outputString().equals("27");
+		numbFails += FractionTest.likeAssert( testPass, DescribeTest, beVerbose);
+
+		lineToProcess = "-3 * 2/3 aB";
+		DescribeTest = "Abs: input of '" + lineToProcess + "' should give '2'. ";
+		testFracCalc.process(lineToProcess);
+		testPass = testFracCalc.outputString().equals("2");
+		numbFails += FractionTest.likeAssert( testPass, DescribeTest, beVerbose);
+
+		lineToProcess = "3/7 N";
+		DescribeTest = "Neg: input of '" + lineToProcess + "' should give '-3/7'. ";
+		testFracCalc.process(lineToProcess);
+		testPass = testFracCalc.outputString().equals("-3/7");
+		numbFails += FractionTest.likeAssert( testPass, DescribeTest, beVerbose);
+
+		lineToProcess = "1/2 - Cl  9 * 7";
+		DescribeTest = "Clear: input of '" + lineToProcess + "' should give '63'. ";
+		testFracCalc.process(lineToProcess);
+		testPass = testFracCalc.outputString().equals("63");
+		numbFails += FractionTest.likeAssert( testPass, DescribeTest, beVerbose);
+
+		lineToProcess = "1/2 - quit 30 + 10";
+		DescribeTest = "Quit: input of '" + lineToProcess + "' should result in quitProgram() true. ";
+		testFracCalc.process(lineToProcess);
+		testPass = testFracCalc.quitProgram();
+		numbFails += FractionTest.likeAssert( testPass, DescribeTest, beVerbose);
+
+
+		lineToProcess = "1/2+1/2";
+		DescribeTest = "Invalid input no space: input of '" + lineToProcess + "' should produce an ERROR. ";
+		testFracCalc.process(lineToProcess);
+		testPass = testFracCalc.foundError();
+		DescribeTest += "Actually '" + testFracCalc.outputString() + "' is output.";
+		numbFails += FractionTest.likeAssert( testPass, DescribeTest, beVerbose);
+
+		lineToProcess = "1/2 + - 3/4";
+		DescribeTest = "Invalid input two operators : input of '" + lineToProcess + "' should produce an ERROR. ";
+		testFracCalc.process(lineToProcess);
+		testPass = testFracCalc.foundError();
+		DescribeTest += "Actually '" + testFracCalc.outputString() + "' is output.";
+		numbFails += FractionTest.likeAssert( testPass, DescribeTest, beVerbose);
+
+		lineToProcess = "1/0";
+		DescribeTest = "Entering an invalid fraction input of '" + lineToProcess + "' should produce an ERROR. ";
+		testFracCalc.process(lineToProcess);
+		testPass = testFracCalc.foundError();
+		DescribeTest += "Actually '" + testFracCalc.outputString() + "' is output.";
+		numbFails += FractionTest.likeAssert( testPass, DescribeTest, beVerbose);
+
+		lineToProcess = "1/2 / 0/3";
+		DescribeTest = "Try to divide by zero '" + lineToProcess + "' should produce an ERROR. ";
+		testFracCalc.process(lineToProcess);
+		testPass = testFracCalc.foundError();
+		DescribeTest += "Actually '" + testFracCalc.outputString() + "' is output.";
+		numbFails += FractionTest.likeAssert( testPass, DescribeTest, beVerbose);
+
+		if (numbFails==0) {
+			if (beVerbose) System.out.println("FractionCalculatorTest: All tests pass");
+			return true;
+		} else {
+			if (beVerbose) System.out.println("FractionCalculatorTest: " + numbFails + " tests FAIL.");
+			assert (false) : "FractionCalculatorTest failed "+ numbFails + " tests";
+			return false;
+		}
+
+
+	}
+
+}
