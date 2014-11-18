020d03c1424a086f88e37fd52f94b057e892c78e
BBK-PiJ-2014-66 <osmart01@dcs.bbk.ac.uk>
2014-11-18 13:41:41 +0000

    start implementing & testing evaluate. Test Output->
    -> Test output shows that some work needs to be done!
    $ java -ea FractionCalculatorTest
    FractionCalculator verbose output:
    Exception in thread "main" java.lang.NullPointerException
    	at FractionCalculatorTest.simpleTests(FractionCalculatorTest.java:28)
    	at FractionCalculatorTest.main(FractionCalculatorTest.java:11)

--- FractionCalculator.java
+++ FractionCalculator.java
@@ -4,8 +4,16 @@
  * date: from 15 Nov 2014
  *  
  * Simple text-based calculator to compute with fractions.
-
-
+ *	
  */
 public class FractionCalculator {
+	public static Fraction evaluate( Fraction fraction, String inputString) {
+		/* The required function. Do not know whether it should be static
+		 * or a class function. Reasonable to assume static as input fraction
+		 * "fraction" must provide the only memory for the the calculator
+                 * state in between line inputs
+                 *
+		 */
+		return null;
+	}
 }
--- FractionCalculatorTest.java
+++ FractionCalculatorTest.java
@@ -3,9 +3,39 @@
  * author: Oliver S. Smart
  * date: from 15 Nov 2014
  *  
- * Runs tests of FractionCalculator function
-
+ * Runs tests of the FractionCalculator function evaluate
  *  
  */ 
 public class FractionCalculatorTest{
+	public static void main( String args[]) {
+		simpleTests( true); // run in verbose mode
+	}
+
+	public static boolean simpleTests( boolean beVerbose) {
+		if (beVerbose)
+                         System.out.println("FractionCalculator verbose output:");
+
+		int numbFails = 0;
+		boolean testPass;
+		String DescribeTest;
+		String lineToProcess; 
+		Fraction initialFraction = new Fraction(0,1);
+		Fraction resultFraction;
+
+		lineToProcess = "1/2 - 3/4 * abs ";
+		DescribeTest = "Extended example table 1st line: '" + lineToProcess + "' should give '1/4'. ";
+		resultFraction = FractionCalculator.evaluate( initialFraction, "lineToProcess");	
+		testPass = resultFraction.equals("1/4");
+		numbFails += FractionTest.likeAssert( testPass, DescribeTest, beVerbose);
+
+		if (numbFails==0) {
+			if (beVerbose) System.out.println("FractionCalculatorTest: All tests pass");
+			return true;
+		} else {
+			if (beVerbose) System.out.println("FractionCalculatorTest: " + numbFails + " tests FAIL.");
+			assert (false) : "FractionCalculatorTest failed "+ numbFails + " tests";
+			return false;
+		}
+
+	}
 }
